750d134bbf8ff62cf0b74a9e6a9438d8c01bb4c4
Yasmim Santos <cavalcantiyasmim213@gmail.com>
2025-09-26 10:29:00 -0300

    criar a tebela alunos

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ conexao = sqlite3.connect("escola.db")
 
 #Criar o objeto chamado de "cursor"  que será usado para execultar os comandos sql
 cursor = conexao.cursor()
+
+#Criar uma tebela no banco de dados 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS alunos (
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER,           
+    curso TEXT
+    )    
+""")
